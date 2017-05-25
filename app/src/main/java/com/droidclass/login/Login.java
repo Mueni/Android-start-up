2a9bd3b66afc5b64bafbe83b20a682e6f30f5d6c
Mueni <4missmusau@gmail.com>
2017-05-25 16:00:42 +0300

    second commit

--- app/src/main/java/com/droidclass/login/Login.java
+++ app/src/main/java/com/droidclass/login/Login.java
@@ -1,7 +1,11 @@
 package com.droidclass.login;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final TextView username = (TextView) findViewById(R.id.username);
+        final TextView password = (TextView) findViewById(R.id.password);
+        Button loginButton = (Button) findViewById(R.id.button_Login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredusername = username.getText().toString();
+                String enteredPassword = password.getText().toString();
+
+                if (enteredusername.equals("")){
+                    username.setError("Email is required");
+                }else if(enteredPassword.equals("")){
+                    password.setError("Password is required!");
+                }else{
+                    startActivity(new Intent(Login.this, Profile.class));
+                }
+            }
+        });
+
     }
 }
