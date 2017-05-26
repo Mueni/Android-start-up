15b5069577df6c0caf23bad0df38cd731d955103
Mueni <4missmusau@gmail.com>
2017-05-26 09:50:35 +0300

    final commit

--- app/src/main/java/com/droidclass/login/Profile.java
+++ app/src/main/java/com/droidclass/login/Profile.java
@@ -1,7 +1,11 @@
 package com.droidclass.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Profile extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button logoutButton = (Button) findViewById(R.id.button_Logout);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Profile.this, Login.class));
+            }
+        });
     }
 }
